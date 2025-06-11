274b4a37e33beb14f27e5ec711b4534956988186
Shan Richards <46514217+ShanNatRichards@users.noreply.github.com>
2025-06-10 17:08:13 -0700

    - For models.py: removed duplication in import statement. - For sample dataset: Updated departments  used in sample dataset.

--- app/backend/simpleapi/myapp/models.py
+++ app/backend/simpleapi/myapp/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
-# Create your models here.
-
-from django.db import models
-
 class Faculty(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
