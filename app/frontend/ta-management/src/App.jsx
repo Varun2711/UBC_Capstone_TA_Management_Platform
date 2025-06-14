59ae9be9b038c99a321ad9b9e1ed1e838aeddadc
Varun Patel <rvjpatel99@gmail.com>
2025-06-13 20:59:38 -0700

    70 setup reverse proxy (#98)
    * setup basic nginx for reverse proxy
    
    * fixed typo in compose file
    
    * checkpoint for nginx changes
    
    * update to dockerfile to remove unnecessary pre-existing nginx files
    
    * finished debugging vite server issues with nginx
    
    * cleaned up and added a few more comments
    
    * added comments in the nginx.conf file

--- app/frontend/ta-management/src/App.jsx
+++ app/frontend/ta-management/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import MockDashboard from "./pages/MockDashboard";
 function App() {
   // define a message state variable
   const [message, setMessage] = useState("");
-  const message_url = "http://localhost:8000/api/hello";
+  const message_url = "/api/hello"; // update the api call to have a relative address instead of a direct address
   // make an api call with fetch
   useEffect(() => {
     fetch(message_url)
@@ -19,7 +19,7 @@ function App() {
         setMessage(data.message);
       })
       .catch((error) => {
-        throw new Error(`error occurred ${error}`);
+        console.log(`error has occurred ${error}`);
       });
   }, []);
 
@@ -27,6 +27,8 @@ function App() {
     // React Router setup, commented out for now
     // Initial test to check router functionlity below
     <Routes>
+      {/*The following message is just for testing purposes*/}
+      {console.log(message)}
       <Route path="/" element={<LoginPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/dashboard" element={<MockDashboard />} />
