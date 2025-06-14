6b4dca69bbc27e3404657cf10eaa01b4edd06836
Ariana Rice <69221845+ariceeee@users.noreply.github.com>
2025-06-14 16:39:09 -0700

    Clean up/style ui

--- app/frontend/ta-management/src/pages/LandingPage.jsx
+++ app/frontend/ta-management/src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { useNavigate } from "react-router-dom";
 
@@ -8,24 +9,32 @@ export function LandingPage() {
     const navigate = useNavigate()
 
     return (
-        <main className="min-h-screen flex flex-col items-center justify-center gap-6 p-4">
-            <h1 className={cn("text-3xl font-extrabold text-center")}>
-                TA Application Portal
-            </h1>
+        <main className="min-h-screen flex flex-col items-left justify-center gap-10 p-10">
+            <div>
+                <h2 className="scroll-m-20 pb-2 text-2xl font-semibold tracking-tight first:mt-0">
+                    Department of Computer Science, Mathematics, Physics and Statistics
+                </h2>
+
+                <h1 className={cn("text-6xl font-extrabold")}>
+                    TA Application Portal
+                </h1>
+            </div>
 
             <div className="flex gap-4">
-            <Button variant="default" onClick = { () => navigate("/login") }>Login</Button>
-            <Button variant="secondary" onClick = { () => navigate("/register") }>Create Account</Button>
+                <Button variant="default" onClick = { () => navigate("/login") }>Login</Button>
+                <Button variant="secondary" onClick = { () => navigate("/register") }>Create An Account</Button>
             </div>
 
             {/* Important Dates Section */}
-            <Card>
+
+            <Card className="w-2/3">
                 <CardHeader>
-                    <CardTitle>Important Dates</CardTitle>
+                    <CardTitle> Active Application Period: Winter 2025 </CardTitle>
+                    <CardDescription> Positions available for Term 1: September - December, and Term 2: January - April </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <p>Applications are open for the 2025W academic period!</p>
-                    <p>Closing date: April 30, 2025</p>
+                    <p>Open date: February 26, 2025</p>
+                    <p>Close date: April 30, 2025</p>
                 </CardContent>
             </Card>
         </main>
