4226bad59cf0094b9ddb024d6c81f4d1e5a00c6d
Devstutya Pandey <97552983+Devstutya@users.noreply.github.com>
2025-06-13 17:59:17 -0700

    Added forgot password page

--- app/frontend/ta-management/src/App.jsx
+++ app/frontend/ta-management/src/App.jsx
@@ -5,6 +5,7 @@ import MockDashboard from "./pages/MockDashboard";
 import CreateAccount1 from "./pages/CreateAccount1";
 import CreateAccount2 from "./pages/CreateAccount2";
 import CreateAccount3 from "./pages/CreateAccount3";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   // define a message state variable
@@ -37,6 +38,7 @@ function App() {
       <Route path="/create-account/step1" element={<CreateAccount1 />} />
       <Route path="/create-account/step2" element={<CreateAccount2 />} />
       <Route path="/create-account/step3" element={<CreateAccount3 />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       {/* Add more routes if needed */}
     </Routes>
     // <TASchedulerDashboard />
