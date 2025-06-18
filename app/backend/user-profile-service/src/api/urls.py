ec6b0bafd2566c4c0334ff2779b34d5fa5f68958
Reyhan-Sam <reyhansam13@gmail.com>
2025-06-18 13:22:59 -0700

    updated nginx to link with user-profile

--- app/backend/user-profile-service/src/api/urls.py
+++ app/backend/user-profile-service/src/api/urls.py
@@ -3,11 +3,13 @@ from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet, InstructorViewSet, TASchedulerViewSet, find_user, api_root
 
 router = DefaultRouter()
+router.include_root_view = False
 router.register(r'students', StudentViewSet)
 router.register(r'instructors', InstructorViewSet)  
 router.register(r'schedulers', TASchedulerViewSet)
 
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('', include(router.urls)),
     path('find-user/', find_user, name='find-user'),
 ]
