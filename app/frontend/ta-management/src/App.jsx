5cede84f39e114b0f6d42e1f296730ef470bb09f
Devstutya Pandey <97552983+Devstutya@users.noreply.github.com>
2025-06-27 14:12:55 -0700

    Added initial admin dashboard

--- app/frontend/ta-management/src/App.jsx
+++ app/frontend/ta-management/src/App.jsx
@@ -6,8 +6,7 @@ import TASchedulerDashboard from "./pages/Scheduler_Dashboard";
 import Application from "./pages/Application";
 import StudentDashboard from "./pages/Student_Dashboard";
 import ProfilePage from "./pages/ProfilePage";
-
-
+import AdminDashboard from "./pages/AdminDashboard";
 import { LandingPage } from "./pages/LandingPage";
 import CreateAccount1 from "./pages/CreateAccount1";
 import CreateAccount2 from "./pages/CreateAccount2";
@@ -42,6 +41,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
       <Route path="/TAdashboard" element={<TASchedulerDashboard />} />
       <Route path="/MockDashboard" element={<MockDashboard />} />
       <Route path="/create-account/step1" element={<CreateAccount1 />} />
