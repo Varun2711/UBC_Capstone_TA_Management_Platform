e58f8212aab8fbfec9a9b929627f4eb4d09545cf
Reyhan-Sam <reyhansam13@gmail.com>
2025-07-08 18:40:44 -0700

    added delete function to view so that course preferrences could be deleted

--- app/backend/user-profile-service/src/api/views.py
+++ app/backend/user-profile-service/src/api/views.py
@@ -408,6 +408,20 @@ class StudentCoursePreferenceListCreateView(generics.ListCreateAPIView):
             user = self.request.user
         serializer.save(user=user)
 
+    def delete(self, request, *args, **kwargs):
+        """Delete all course preferences for the user"""
+        student_id = self.kwargs.get('student_id')
+        if student_id:
+            student = get_object_or_404(Student, student_number=student_id)
+            user = get_object_or_404(User, email=student.email)
+        else:
+            user = self.request.user
+        
+        deleted_count = StudentCoursePreference.objects.filter(user=user).delete()[0]
+        return Response({
+            'message': f'Deleted {deleted_count} course preferences successfully'
+        }, status=status.HTTP_200_OK)
+
 class StudentCoursePreferenceDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = StudentCoursePreferenceSerializer
     permission_classes = [IsAuthenticated]
