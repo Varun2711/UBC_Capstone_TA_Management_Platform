ab8476c5d7e8ac33c85cfbaf55732d4cb16c862d
Varun Patel <rvjpatel99@gmail.com>
2025-07-11 10:59:40 -0700

    update model name in the job postings

--- app/backend/applications-jobpostings-service/src/api/models.py
+++ app/backend/applications-jobpostings-service/src/api/models.py
@@ -89,7 +89,7 @@ class Term(models.Model):
 
     class Meta:
         managed = False
-        db_table = 'academic_terms'  
+        db_table = 'myapp_academic_terms'  
       
 
     def __str__(self):
