06455d9a69aa51e2c49870319bd5034aea25da30
Ariana Rice <69221845+ariceeee@users.noreply.github.com>
2025-07-22 10:53:37 -0700

    Update reset password route

--- app/frontend/ta-management/src/App.jsx
+++ app/frontend/ta-management/src/App.jsx
@@ -16,7 +16,7 @@ import { LandingPage } from "./pages/LandingPage";
 import CreateAccount1 from "./pages/CreateAccount1";
 import CreateAccount2 from "./pages/CreateAccount2";
 import CreateAccount3 from "./pages/CreateAccount3";
-import ForgotPassword from "./pages/ForgotPassword";
+import ResetPasswordController from "./pages/ResetPassword/ResetPasswordController";
 import UserProfile from "./pages/profile-page-scheduler";
 import TaCoordinatorAllocationPage from "./pages/TaCoordinatorAllocationPage";
 import ViewJobPostings from "./pages/Student/Student_ViewJobPostings";
@@ -40,7 +40,7 @@ function App() {
       <Route path="/create-account/step1" element={<CreateAccount1 />} />
       <Route path="/create-account/step2" element={<CreateAccount2 />} />
       <Route path="/create-account/step3" element={<CreateAccount3 />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/forgot-password" element={<ResetPasswordController />} />
 
       {/* Accessible to: student ---------------- */}
       <Route
