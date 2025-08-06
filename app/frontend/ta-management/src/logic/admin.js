7863006f16f82d389b46f28946da607a99c23536
Devstutya Pandey <97552983+Devstutya@users.noreply.github.com>
2025-08-06 11:25:29 -0700

    Added services to system health check

--- app/frontend/ta-management/src/logic/admin.js
+++ app/frontend/ta-management/src/logic/admin.js
@@ -113,7 +113,10 @@ export const getSystemHealth = async () => {
       { name: 'User Profile Service', url: `${API_URL}/api/profile/` },
       { name: 'Notification Service', url: `${API_URL}/api/notifications/` },
       { name: 'Allocations Service', url: `${API_URL}/api/allocations/` },
-      { name: 'Auth Service', url: `${API_URL}/api/auth/` }
+      { name: 'Auth Service', url: `${API_URL}/api/auth/` },
+      { name: 'Applications JobPostings Service', url: `${API_URL}/api/ajp/` },
+      { name: 'Course Service', url: `${API_URL}/api/course-term-service/` },
+      { name: 'Simple API Service', url: `${API_URL}/api/` }
     ];
     
     const serviceChecks = await Promise.allSettled(
